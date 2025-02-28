232241d3637d9311dc726ad571bfa92b1bf3e79f
kalebe <kalebe@DESKTOP-FQUTAG8>
2025-02-27 22:42:13 -0300

    commit3

--- src/atividade/atividade1.java
+++ src/atividade/atividade1.java
@@ -4,6 +4,40 @@ public class atividade1 {
 
 	public static void main(String[] args) {
 
+		int [][] matrizA = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
+		
+		int [][] matrizB = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
+		
+		int [] [] soma = new int [3] [3];
+		
+		for (int i = 0; i < 3; i++) {
+			
+			for (int l = 0; l < 3; l++) {
+				
+				soma[i] [l] = matrizA [i] [l] + matrizB [i] [l];
+				
+			}
+		}
+		
+		
+		for (int i = 0; i < 3; i++) {
+			
+			for(int l = 0; l < 3; l++) {
+				
+				System.out.print(soma[i] [l] + " ");
+				
+			}
+			
+			System.out.println();	
+		}
 		
 		
 	}
